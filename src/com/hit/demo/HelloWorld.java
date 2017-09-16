13db87d482ced7a47d6147237e2faa261fbde799
jianhonghua <1149115536@qq.com>
2017-09-16 20:24:10 +0800

    hello intellij IDEA

--- src/com/hit/demo/HelloWorld.java
+++ src/com/hit/demo/HelloWorld.java
@@ -2,6 +2,7 @@ package com.hit.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("hello intellij IEDA");
         System.out.println("hello world");
     }
 }
